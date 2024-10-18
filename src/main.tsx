42093bbc12210812fbf64f698d5a8d5bb4c98f2a
RED <soungbeom@gmail.com>
2024-10-18 16:24:59 +0900

    ..

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"
 import App from './App.tsx'
 import WebApp from '@twa-dev/sdk'
 import './index.css'
