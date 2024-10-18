b12c9048ff0165b389d27004929cd2158f3a9bbf
RED <soungbeom@gmail.com>
2024-10-18 16:07:05 +0900

    ..

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import App from './App.tsx'
+import WebApp from '@twa-dev/sdk'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+WebApp.ready();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
